3e4a7c9b5f91afd418b51e556cdd6e341ce84370
Jason Fieldman <jason@fieldman.org>
2016-02-11 16:13:28 -0800

    more tests

--- MortarTests/MortarTests.swift
+++ MortarTests/MortarTests.swift
@@ -46,8 +46,18 @@ class MortarTests: XCTestCase {
         
         self.container |+| v
         
-        /* Installs 4 constraints (one for each edge) */
         v.m_edges |=| self.container
+        
+        XCTAssertEqual(self.container.constraints.count, 4, "Should have 4 constraints installed (ancestor)")
+        XCTAssertEqual(v.constraints.count, 0, "Should have 0 constraints installed (constraints installed on ancestor)")
+    }
+    
+    func testArrayConstruction() {
+        let v = MortarView()
+        
+        self.container |+| v
+        
+        v |=| [self.container.m_caps, self.container.m_sides]
         
         XCTAssertEqual(self.container.constraints.count, 4, "Should have 4 constraints installed (ancestor)")
         XCTAssertEqual(v.constraints.count, 0, "Should have 0 constraints installed (constraints installed on ancestor)")
@@ -58,7 +68,6 @@ class MortarTests: XCTestCase {
         
         self.container |+| v
         
-        /* Installs 4 constraints (one for each edge) */
         let c = v.m_edges |=| self.container
         
         XCTAssertEqual(self.container.constraints.count, 4, "Should have 4 constraints installed (ancestor)")
@@ -74,13 +83,43 @@ class MortarTests: XCTestCase {
         XCTAssertEqual(self.container.constraints.count, 4, "Should have 4 constraints installed (activated)")
         XCTAssertEqual(v.constraints.count, 0, "Should have 0 constraints installed (constraints installed on ancestor)")
     }
+    
+    func testBasicGroup() {
+        let v = MortarView()
+        
+        self.container |+| v
+        
+        let g = [
+            v.m_sides |=| self.container,
+            v.m_caps  |=| self.container,
+        ]
+        
+        XCTAssertEqual(self.container.constraints.count, 4, "Should have 4 constraints installed (ancestor)")
+        XCTAssertEqual(v.constraints.count, 0, "Should have 0 constraints installed (constraints installed on ancestor)")
+        
+        g.deactivate()
+        
+        XCTAssertEqual(self.container.constraints.count, 0, "Should have 0 constraints installed (deactivated)")
+        XCTAssertEqual(v.constraints.count, 0, "Should have 0 constraints installed (constraints installed on ancestor)")
+        
+        g.activate()
+        
+        XCTAssertEqual(self.container.constraints.count, 4, "Should have 4 constraints installed (activated)")
+        XCTAssertEqual(v.constraints.count, 0, "Should have 0 constraints installed (constraints installed on ancestor)")
+        
+        self.container.layoutIfNeeded()
+        
+        XCTAssertEqual(v.frame.origin.x, 0, "Frame mismatch")
+        XCTAssertEqual(v.frame.origin.y, 0, "Frame mismatch")
+        XCTAssertEqual(v.frame.size.width,  MortarTests.CON_W, "Frame mismatch")
+        XCTAssertEqual(v.frame.size.height, MortarTests.CON_H, "Frame mismatch")
+    }
 
     func testBasicFrame() {
         let v = MortarView()
         
         self.container |+| v
         
-        /* Installs 4 constraints (one for each edge) */
         v.m_edges |=| self.container
         
         self.container.layoutIfNeeded()
@@ -89,6 +128,103 @@ class MortarTests: XCTestCase {
         XCTAssertEqual(v.frame.origin.y, 0, "Frame mismatch")
         XCTAssertEqual(v.frame.size.width,  MortarTests.CON_W, "Frame mismatch")
         XCTAssertEqual(v.frame.size.height, MortarTests.CON_H, "Frame mismatch")
+    }
+    
+    func testAddSubviews() {
+        let v1 = MortarView()
+        let v2 = MortarView()
+        let v3 = MortarView()
+        let v4 = MortarView()
+        
+        self.container |+| [
+            v1,
+            v2 |+| [
+                v3,
+                v4
+            ]
+        ]
+        
+        XCTAssertEqual(v1.superview, self.container, "Hierarchy mismatch")
+        XCTAssertEqual(v2.superview, self.container, "Hierarchy mismatch")
+        XCTAssertEqual(v3.superview, v2, "Hierarchy mismatch")
+        XCTAssertEqual(v4.superview, v2, "Hierarchy mismatch")
+        
+    }
+    
+    func testBasicInset() {
+        let v = MortarView()
+        
+        self.container |+| v
+        
+        v |=| self.container.m_edges ~ (10, 10, 10, 10)
+        
+        self.container.layoutIfNeeded()
+        
+        XCTAssertEqual(v.frame.origin.x, 10, "Frame mismatch")
+        XCTAssertEqual(v.frame.origin.y, 10, "Frame mismatch")
+        XCTAssertEqual(v.frame.size.width,  MortarTests.CON_W - 20, "Frame mismatch")
+        XCTAssertEqual(v.frame.size.height, MortarTests.CON_H - 20, "Frame mismatch")
+    }
+    
+    func testBasicMult() {
+        let v = MortarView()
+        
+        self.container |+| v
+        
+        v |=| self.container.m_height * 2
+        
+        self.container.layoutIfNeeded()
+        
+        XCTAssertEqual(v.frame.size.height, MortarTests.CON_H * 2, "Frame mismatch")
+    }
+    
+    func testBasicConst() {
+        let v = MortarView()
+        
+        self.container |+| v
+        
+        v |=| self.container.m_height + 5
+        
+        self.container.layoutIfNeeded()
+        
+        XCTAssertEqual(v.frame.size.height, MortarTests.CON_H + 5, "Frame mismatch")
+    }
+    
+    func testBasicCombo() {
+        let v = MortarView()
+        
+        self.container |+| v
+        
+        v |=| self.container.m_height * 2 + 5
+        
+        self.container.layoutIfNeeded()
+        
+        XCTAssertEqual(v.frame.size.height, MortarTests.CON_H * 2 + 5, "Frame mismatch")
+    }
+    
+    func testBasicPriority() {
+        let v0 = MortarView()
+        let v1 = MortarView()
+        let v2 = MortarView()
+        let v3 = MortarView()
+        let v4 = MortarView()
+        let v5 = MortarView()
+        
+        self.container |+| [v0, v1, v2, v3, v4, v5]
+        
+        v0 |=| self.container.m_height
+        v1 |=| self.container.m_height ! .Low
+        v2 |=| self.container.m_height ! .Normal
+        v3 |=| self.container.m_height ! .High
+        v4 |=| self.container.m_height ! .Required
+        v5 |=| self.container.m_height ! 300
+        
+        XCTAssertEqual(self.container.constraints[0].priority, UILayoutPriorityDefault,     "Priority mismatch")
+        XCTAssertEqual(self.container.constraints[1].priority, UILayoutPriorityDefaultLow,  "Priority mismatch")
+        XCTAssertEqual(self.container.constraints[2].priority, UILayoutPriorityDefault,     "Priority mismatch")
+        XCTAssertEqual(self.container.constraints[3].priority, UILayoutPriorityDefaultHigh, "Priority mismatch")
+        XCTAssertEqual(self.container.constraints[4].priority, UILayoutPriorityRequired,    "Priority mismatch")
+        XCTAssertEqual(self.container.constraints[5].priority, 300,                         "Priority mismatch")
     }
     
 }
