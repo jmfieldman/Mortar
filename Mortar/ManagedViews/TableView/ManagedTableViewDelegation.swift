6c67d7bfe5727008fa2e216f760bbaae06dee3c3
Jason Fieldman <jason@fieldman.org>
2025-10-02 08:07:51 -0700

    cast to table view for delegates

--- Mortar/ManagedViews/TableView/ManagedTableViewDelegation.swift
+++ Mortar/ManagedViews/TableView/ManagedTableViewDelegation.swift
@@ -19,96 +19,96 @@ public extension ManagedTableView {
 
 public final class _ManagedTableViewScrollDelegateHandler: NSObject {
     /// Called when the user scrolls the content view.
-    public var didScroll: ((UIScrollView) -> Void)?
+    public var didScroll: ((ManagedTableView) -> Void)?
 
     /// Called when the user scrolls to the top of the content view.
-    public var didScrollToTop: ((UIScrollView) -> Void)?
+    public var didScrollToTop: ((ManagedTableView) -> Void)?
 
     /// Asks the delegate if the scroll view should scroll to the top.
-    public var shouldScrollToTop: ((UIScrollView) -> Bool)?
+    public var shouldScrollToTop: ((ManagedTableView) -> Bool)?
 
     /// Called when the user finishes zooming.
-    public var didZoom: ((UIScrollView) -> Void)?
+    public var didZoom: ((ManagedTableView) -> Void)?
 
     /// Called when the user begins zooming.
-    public var willBeginZooming: ((UIScrollView, UIView?) -> Void)?
+    public var willBeginZooming: ((ManagedTableView, UIView?) -> Void)?
 
     /// Called when the user finishes zooming.
-    public var didEndZooming: ((UIScrollView, UIView?, CGFloat) -> Void)?
+    public var didEndZooming: ((ManagedTableView, UIView?, CGFloat) -> Void)?
 
     /// Called when the user begins dragging.
-    public var willBeginDragging: ((UIScrollView) -> Void)?
+    public var willBeginDragging: ((ManagedTableView) -> Void)?
 
     /// Called when the user ends dragging.
-    public var willEndDragging: ((UIScrollView, CGPoint, UnsafeMutablePointer<CGPoint>) -> Void)?
+    public var willEndDragging: ((ManagedTableView, CGPoint, UnsafeMutablePointer<CGPoint>) -> Void)?
 
     /// Called when the user ends dragging.
-    public var didEndDragging: ((UIScrollView, Bool) -> Void)?
+    public var didEndDragging: ((ManagedTableView, Bool) -> Void)?
 
     /// Called when the user begins decelerating.
-    public var willBeginDecelerating: ((UIScrollView) -> Void)?
+    public var willBeginDecelerating: ((ManagedTableView) -> Void)?
 
     /// Called when the user ends decelerating.
-    public var didEndDecelerating: ((UIScrollView) -> Void)?
+    public var didEndDecelerating: ((ManagedTableView) -> Void)?
 
     /// Called when the scroll view ends scrolling animation.
-    public var didEndScrollingAnimation: ((UIScrollView) -> Void)?
+    public var didEndScrollingAnimation: ((ManagedTableView) -> Void)?
 
     /// Called when the scroll view's adjusted content inset changes.
-    public var didChangeAdjustedContentInset: ((UIScrollView) -> Void)?
+    public var didChangeAdjustedContentInset: ((ManagedTableView) -> Void)?
 }
 
 extension ManagedTableView: UIScrollViewDelegate {
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.didScroll?(scrollView)
+        scrollDelegateHandler.didScroll?(scrollView as! ManagedTableView)
     }
 
     public func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.didScrollToTop?(scrollView)
+        scrollDelegateHandler.didScrollToTop?(scrollView as! ManagedTableView)
     }
 
     public func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
-        scrollDelegateHandler.shouldScrollToTop?(scrollView) ?? true
+        scrollDelegateHandler.shouldScrollToTop?(scrollView as! ManagedTableView) ?? true
     }
 
     public func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
-        scrollDelegateHandler.willBeginZooming?(scrollView, view)
+        scrollDelegateHandler.willBeginZooming?(scrollView as! ManagedTableView, view)
     }
 
     public func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.didZoom?(scrollView)
+        scrollDelegateHandler.didZoom?(scrollView as! ManagedTableView)
     }
 
     public func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
-        scrollDelegateHandler.didEndZooming?(scrollView, view, scale)
+        scrollDelegateHandler.didEndZooming?(scrollView as! ManagedTableView, view, scale)
     }
 
     public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.willBeginDragging?(scrollView)
+        scrollDelegateHandler.willBeginDragging?(scrollView as! ManagedTableView)
     }
 
     public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        scrollDelegateHandler.willEndDragging?(scrollView, velocity, targetContentOffset)
+        scrollDelegateHandler.willEndDragging?(scrollView as! ManagedTableView, velocity, targetContentOffset)
     }
 
     public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        scrollDelegateHandler.didEndDragging?(scrollView, decelerate)
+        scrollDelegateHandler.didEndDragging?(scrollView as! ManagedTableView, decelerate)
     }
 
     public func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.willBeginDecelerating?(scrollView)
+        scrollDelegateHandler.willBeginDecelerating?(scrollView as! ManagedTableView)
     }
 
     public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.didEndDecelerating?(scrollView)
+        scrollDelegateHandler.didEndDecelerating?(scrollView as! ManagedTableView)
     }
 
     public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.didEndScrollingAnimation?(scrollView)
+        scrollDelegateHandler.didEndScrollingAnimation?(scrollView as! ManagedTableView)
     }
 
     public func scrollViewDidChangeAdjustedContentInset(_ scrollView: UIScrollView) {
-        scrollDelegateHandler.didChangeAdjustedContentInset?(scrollView)
+        scrollDelegateHandler.didChangeAdjustedContentInset?(scrollView as! ManagedTableView)
     }
 }
 
